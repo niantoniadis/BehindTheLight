41bb7d7c62fb2c64a87564acb653447d4437d09c
jkim99 <jonathanskim@gmail.com>
2019-11-02 01:03:37 -0400

    quick update

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    public EnemySpawner spawner;
+    public List<EnemySpawner> spawners;
+    public List<Enemy> allEnemies;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawners = new List<EnemySpawner>();
+        allEnemies = new List<Enemy>();
+        spawners.Add(Instantiate(spawner.gameObject, new Vector3(10, 0, 0), Quaternion.identity).GetComponent<EnemySpawner>());
+        spawners.Add(Instantiate(spawner.gameObject, new Vector3(-10, 0, 0), Quaternion.identity).GetComponent<EnemySpawner>());
+        spawners.Add(Instantiate(spawner.gameObject, new Vector3(0, 10, 0), Quaternion.identity).GetComponent<EnemySpawner>());
+        spawners.Add(Instantiate(spawner.gameObject, new Vector3(0, -10, 0), Quaternion.identity).GetComponent<EnemySpawner>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(EnemySpawner e in spawners)
+        {
+
+        }        
     }
 }
