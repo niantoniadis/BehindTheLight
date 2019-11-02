c93d262c07dfc57216fa16b51ae1b3c669f20e22
niantoniadis <naa3207@rit.edu>
2019-11-01 22:42:27 -0400

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 direction;
+    Vector3 position;
+    Vector3 velocity;
+    Vector3 desiredVelocity;
+    Vector3 acceleration;
+    float angleOfRotation;
+    float mass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void RotateVehicle()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(Input.mousePosition.x - position.x, Input.mousePosition.y - position.y));
+    }
+
+    public void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+}
---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    Vector3 position;
+    Vector3 velocity;
+    Vector3 desiredVelocity;
+    Vector3 acceleration;
+    float maxSpeed;
+    float rotation;
+    int mass;
+    bool seeking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeking = true;
+        mass = 10;
+
+        maxSpeed = 5f;
+
+        velocity = Vector3.zero;
+        acceleration = Vector3.zero;
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (position.x < -8.887f + GetComponent<SpriteRenderer>().bounds.size.x/2)
+        {
+            acceleration.x = -acceleration.x;
+            velocity.x = -velocity.x;
+        }
+        if (position.x > 8.887f - GetComponent<SpriteRenderer>().bounds.size.x / 2)
+        {
+            acceleration.x = -acceleration.x;
+            velocity.x = -velocity.x;
+        }
+        if (position.y < -5f + GetComponent<SpriteRenderer>().bounds.size.y / 2)
+        {
+            acceleration.y = -acceleration.y;
+            velocity.y = -velocity.y;
+        }
+        if (position.y > 5f - GetComponent<SpriteRenderer>().bounds.size.y / 2)
+        {
+            acceleration.y = -acceleration.y;
+            velocity.y = -velocity.y;
+        }
+
+        velocity += acceleration * Time.deltaTime;
+        position += velocity * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotation - 90f);
+        transform.position = position;
+
+        acceleration = Vector3.zero;
+    }
+
+    public bool Seeking
+    {
+        get
+        {
+            return seeking;
+        }
+        set
+        {
+            seeking = value;
+        }
+    }
+
+    public void Flee(Vehicle target)
+    {
+        desiredVelocity = position - target.position;
+        desiredVelocity = Vector3.ClampMagnitude(desiredVelocity, maxSpeed);
+        acceleration += (desiredVelocity - velocity) / mass;
+    }
+
+    public void Seek(Vehicle target)
+    {
+        desiredVelocity = target.position - position;
+        desiredVelocity = Vector3.ClampMagnitude(desiredVelocity, maxSpeed);
+        acceleration += ((desiredVelocity - velocity) / mass);
+    }
+
+    public bool IsColliding(SpriteRenderer colliding)
+    {
+        if (colliding.bounds.size.x/2 + GetComponent<SpriteRenderer>().bounds.size.x/2 > Vector3.Distance(colliding.bounds.center, GetComponent<SpriteRenderer>().bounds.center))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Teleport()
+    {
+        position = new Vector3(Random.Range(-8.5f, 8.5f), Random.Range(-4.5f, 4.5f), 0);
+        transform.position = position;
+    }
+
+    public void SetRotation(Vehicle target)
+    {
+        Vector3 direction;
+        if (seeking)
+        {
+            direction = target.position - position;
+            direction.Normalize();
+            rotation = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        }
+        else
+        {
+            direction = position - target.position;
+            direction.Normalize();
+            rotation = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        }
+    }
+}
